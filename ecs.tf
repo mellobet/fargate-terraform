4742fd903f0e6248c29cf79f12d65621446357b0
Mario Llobet <mario.llobet@onapsis.com>
2020-07-28 14:41:00 -0300

    added ALB

--- ecs.tf
+++ ecs.tf
@@ -28,13 +28,32 @@ resource "aws_ecs_service" "ecs_service" {
         security_groups = [aws_security_group.ecs_security_group.id]
         assign_public_ip = true
     }
+
+    # # Load Balancer
+    # load_balancer {
+    #     container_name      = "nginx"
+    #     container_port      = 80
+    #     target_group_arn    = aws_alb_target_group.ecs_app_target_group.arn
+    # }
+
 }
 
-
+# Create a Target Group (with default Health Checks)
 resource "aws_alb_target_group" "ecs_app_target_group" {
     name        = "${var.ecs_service_name}-TG"
-    port        = 80 # Req when using type="ip"
-    protocol    = "HTTP" # Req when using type="ip"
+    port        = 80        # Req when using type="ip"
+    protocol    = "HTTP"    # Req when using type="ip"
     vpc_id      = aws_vpc.tf-main-vpc.id
-    target_type = "ip" # awsvpc only allows "ip"
+    target_type = "ip"      # awsvpc only allows "ip"
 }
+
+
+# Create ALB
+resource "aws_alb" "ecs_cluster_alb" {
+    name            = "${var.ecs_cluster_name}-ALB"
+    internal        = false
+    security_groups = [aws_security_group.ecs_security_group.id]
+    subnets         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+# Create Listener
