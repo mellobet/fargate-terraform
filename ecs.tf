fc90740264067bab0cb6958537fa555cfbabb42d
Mario Llobet <mario.llobet@onapsis.com>
2020-07-31 23:12:05 -0300

    added comment

--- ecs.tf
+++ ecs.tf
@@ -25,6 +25,7 @@ resource "aws_ecs_service" "ecs_service" {
 
     # Required when Fargate(awsvpc).
     network_configuration {
+        # TODO: just one network???!!!!!!
         subnets = [aws_subnet.public_subnet_1.id]
         security_groups = [aws_security_group.ecs_security_group.id]
         assign_public_ip = true
