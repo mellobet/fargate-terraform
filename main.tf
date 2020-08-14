b0440712f05c1d97a8e93d055ba47cc2c8823327
Mario Llobet <mario.llobet@onapsis.com>
2020-08-14 12:41:55 -0300

    added token comment

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 # Define the region
 # $ export AWS_ACCESS_KEY_ID="anaccesskey"
 # $ export AWS_SECRET_ACCESS_KEY="asecretkey"
+# $ export AWS_SESSION_TOKEN="token"
 # $ export AWS_DEFAULT_REGION="us-east-1"
 provider "aws" {
   region  = var.region
